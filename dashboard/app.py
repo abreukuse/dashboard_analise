9ff9a8d828ba23f1911b7d85f7932011ca4edf6d
Kuse <tobias.abreu.kuse@gmail.com>
2021-09-24 22:27:27 -0300

    Adicionei o calculo da base

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,12 +1,15 @@
 # import os
 import streamlit as st
 import pandas as pd
+import numpy as np
 from tabelas.tabela_cruzamentos import criar_tabela
 from plotnine import (ggplot, aes, facet_wrap, 
                       geom_bar, coord_flip,
                       theme, geom_text, element_blank, 
                       after_stat, element_text, element_rect, 
                       position_dodge)
+
+from iteround import saferound
 
 import plotnine
 from plotnine.themes import theme_bw
@@ -57,6 +60,17 @@ def tabela_formato_longo(tabela, variavel, cruzamento):
     return table
 
 
+@st.cache(show_spinner=False)
+def calcular_base(dados, cruzamento):
+    parcial = dados.groupby(cruzamento)['peso'].sum()
+    total = np.sum(parcial)
+    base = pd.Series(saferound((parcial/total)*100, 0), 
+                     index=parcial.index,
+                     name='Base').astype('int32').reset_index()
+
+    base['Base'] = base['Base'].apply(lambda x: f'Base: {x}%')
+    return base
+
 def grafico_barra(tabela, variavel, mostrar_porcentagem):
 
 	# Pegar o tamanho da maior string para ajustar as labels
@@ -76,6 +90,8 @@ def grafico_barra(tabela, variavel, mostrar_porcentagem):
 
     figura = (
                 ggplot(tabela, aes(x=variavel, y='Total'))
+
+                + theme_bw(base_family='Montserrat')
 
                 + geom_bar(stat='identity', fill='#1fa8a9', width=0.4, alpha=0.85)
 
@@ -126,14 +142,19 @@ def grafico_facetado(tabela, variavel, cruzamento, mostrar_porcentagem):
     tabela = ajustes_tabela(tabela)
     tabela = tabela_formato_longo(tabela, variavel, cruzamento)
 
+    base = calcular_base(DATAFRAME, cruzamento)
+    tabela = tabela.merge(base, on=cruzamento)
+    
     format_string = '{}%' if mostrar_porcentagem else '{}'
 
     figura = (
                 ggplot(tabela, aes(x=variavel, y='Valores'))
 
+                + theme_bw(base_family='Montserrat')
+
                 + geom_bar(stat='identity', fill='#1fa8a9', width=0.4, alpha=0.85)
 
-                + facet_wrap(f'~{cruzamento}', ncol=n_cols)
+                + facet_wrap(f'~{cruzamento} + Base', ncol=n_cols)
 
                 + coord_flip()
 
@@ -147,7 +168,7 @@ def grafico_facetado(tabela, variavel, cruzamento, mostrar_porcentagem):
                                                     ha='center', 
                                                     size=10, 
                                                     margin={'r':maior_label*3}
-                                                ),            
+                                                ),           
                         )
                 
                 + geom_text(
